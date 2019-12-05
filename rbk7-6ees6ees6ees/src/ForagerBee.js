98a9a0440fe6269c20de0d678a097794820cf77c
AmeraAbdallah <amera.ayad.it@gmail.com>
2019-12-05 13:49:41 +0100

    12 5

--- rbk7-6ees6ees6ees/src/ForagerBee.js
+++ rbk7-6ees6ees6ees/src/ForagerBee.js
@@ -1,3 +1,14 @@
-class ForagerBee {
-  // TODO..
+class ForagerBee extends Bee {
+  
+  constructor(age = 10, job = "find pollen", canFly = true) {
+  	super();
+  	this.age = age;
+  	this.job = job;
+  	this.canFly = canFly;
+  	this.treasureChest = [];
+  }
+
+  forage(treasure) {
+  	this.treasureChest.push(treasure);
+  }
 };
